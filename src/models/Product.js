4994412a9bca11ff0c08fb0b9a2913388d114df6
Alejandro Zuleta <alejozuleta9217@gmail.com>
2025-02-20 09:19:55 -0500

    cargue del backend prueba tecnica tienda

--- src/models/Product.js
+++ src/models/Product.js
@@ -2,10 +2,16 @@
 const mongoose = require("mongoose");
 
 const productoSchema = new mongoose.Schema({
-  nombre: String,
-  descripcion: String,
-  precio: Number,
+  name: String,         
+  price: Number,        
+  category: String,
   stock: Number,
+  description: String,  
+  brand: String,
+  sku: String,
+  tags: [String],       
+  createdAt: Date,
+  updatedAt: Date,
 });
 
 module.exports = mongoose.model("Producto", productoSchema);
